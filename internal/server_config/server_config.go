f650103a9b03ed53fd9f1bf73e5743b4ad4173ae
Евгений Амплеев <eampleev@192.168.1.4>
2025-06-24 22:21:25 +0300

    fix db dsn to server prod

--- internal/server_config/server_config.go
+++ internal/server_config/server_config.go
@@ -29,7 +29,7 @@ func (c *ServerConfig) SetValues() {
 	flag.StringVar(&c.LogLevel, "l", "debug", "logger level")
 	// принимаем строку подключения к базе данных
 	//flag.StringVar(&c.DBDSN, "d", "postgresql://raya-local:raya-local@localhost:5432/raya_local_corp?sslmode=disable", "postgres database")
-	flag.StringVar(&c.DBDSN, "d", "postgresql://raya_prod:raya_prod@localhost:5432/raya_prod?sslmode=disable", "postgres database")
+	flag.StringVar(&c.DBDSN, "d", "postgresql://raya_prod:Newpass34,@localhost:5432/raya_prod?sslmode=disable", "postgres database")
 	// принимаем секретный ключ сервера для авторизации
 	flag.StringVar(&c.SecretKey, "s", "e4853f5c4810101e88f1898db21c15d3", "server's secret key for authorization")
 
